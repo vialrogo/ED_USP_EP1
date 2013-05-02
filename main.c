a4ae6c4e36827729276a7ec8b7807bcf6590b0d0
Victor Alberto Romero <vialrogo@gmail.com>
2013-05-02 09:48:14 -0300

    Lectura de archivos funcionando

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
+FILE* file;
+
 int main(int argc, char *argv[])
 {
-    printf("Os carateres foram:\n");
+    char* NomeArquivoEntrada;
+    char* NomeArquivoSaida;
+
+    NomeArquivoEntrada = argv[1];
+    NomeArquivoSaida = argv[2];
+
+    /* Leitura do arquivo*/
     
-    int i;
-    for(i=1; i<argc; i++)
+    int i = 0;
+    
+    file = fopen (NomeArquivoEntrada,"r");
+    fscanf (file, "%d", &i);
+
+    while(!feof (file))
     {
-        printf("%s\n", argv[i]);
+        printf("%d\n", i);
+        fscanf (file, "%d", &i);
     }
 
+
+    fclose(file);
     return 0;
 }
